70ce572a8bd049d9ce6b44cf1d979caeb6eaeec2
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2024-11-20 18:42:56 -0700

    did better than yt but took a long time

--- Leetcode/Easy/movezeroes.py
+++ Leetcode/Easy/movezeroes.py
@@ -1,15 +1,20 @@
 nums = [0,1,0,3,12]
 l = 0
-r = 1
-while r <= len(nums)-1:
-    if nums[l] == 0:
-        if nums[r] != 0:
-            nums[l], nums[r] = nums[r], 0
-            l += 1
-            r += 1
-        else: 
-            r += 1
-    else:
+
+# Best Solution
+for r in range(len(nums)):
+    if nums[r]:
+        nums[l], nums[r] = nums[r], 0
         l += 1
-        r += 1
-print(nums)
+    
+
+# r = 1
+# while r <= len(nums)-1:
+#     if nums[l] == 0:
+#         if nums[r] != 0:
+#             nums[l], nums[r] = nums[r], 0
+#             l += 1
+#     else:
+#         l += 1
+#     r += 1
+# print(nums)
---
+++ Leetcode/Medium/ZigZag Conversion.py
@@ -0,0 +1,44 @@
+s = "ABC"
+numRows = 1
+matrix = []
+# for i in range(numRows):
+#     matrix.append("")
+# p = 0
+# reverse = False
+# for i in s:
+#     matrix[p] += i
+#     if reverse:
+#         p -= 1
+#     else:
+#         p += 1
+#         if p >= numRows:
+#             reverse = True
+#             if p < 2:
+#                 p = 0
+#             else:
+#                 p -= 2
+#     if p == 0:
+#         reverse = False
+    
+    
+print("".join(matrix))
+# while a < len(s):
+#     if i == p:
+#         matrix[i][j] == s[a]
+#         a += 1
+#         if i == numRows - 1 or p == numRows - 1:
+#             p -= 1
+#             i = 0
+#             j += 1
+#         else:
+#             i = 0
+#             p -= 1
+#             j += 1
+#             while p != 0:    
+#                 if i == p:
+#                     matrix[i][j] == s[a]
+#                     a += 1
+#     elif i != p:
+#         matrix[i][j] == 0
+# print(matrix)
+  
